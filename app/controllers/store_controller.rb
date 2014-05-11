bba056c9c3e8cd57103d37203af5f4e1187f2967
Freika <iamfrey@gmail.com>
2014-05-11 20:08:33 +0700

    Fixed: line items quantity in cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,5 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    if session[:counter].nil?
-      session[:counter]
-    else
-      session[:counter] = session[:counter] + 1
-    end
   end
 end
