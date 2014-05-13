c88f22e059c6daa9ec3ac3956e089e5b434cb629
Freika <iamfrey@gmail.com>
2014-05-13 21:44:23 +0700

    Added: mail

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,29 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "Frey <depot@example.com>"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.recieved.subject
+  #
+  def recieved(order)
+    @order = order
+    @greeting = "Hi"
+
+    mail to: order.email, subject: "Подтверждение заказа в Pragmatic Store"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+
+    @greeting = "Hi"
+
+    mail to: order.email, subject: "Заказ отправлен"
+  end
+end
